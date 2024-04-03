5e8df6363d0ed7fd7a92652a975c5c4b1d1fb750
Khoa Nguyen <henrykhoanguyen@gmail.com>
2024-04-02 22:26:15 -0500

    initial commit

---
+++ src/main/java/com/take/home/ObjectDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.take.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/take/home/ObjectDetection/ObjectDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.take.home.ObjectDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
